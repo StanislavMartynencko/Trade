5b94b6ac9aa5fafcda7db5f7dc44f54c34e7fafc
StanislavMartynencko <ctys7777@gmail.com>
2018-06-15 08:54:48 +0300

    New stage

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -12,22 +12,15 @@ public class Controller {
     public static Label money;
     @FXML
     public void onClickAddPact(){
-        /*Parent root = null;
+        Parent root = null;
         try {
             Stage stage = new Stage();
             root = FXMLLoader.load(getClass().getResource("AddPact.fxml"));
-            stage.setTitle("Заключение договора ");
+            stage.setTitle("New Contract");
             stage.setScene(new Scene(root, 300, 275));
-            money.setText("1000");
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
-        }*/
-        money = new Label();
-        money.setText("1000");
+        }
     }
-
-
-
-
 }
